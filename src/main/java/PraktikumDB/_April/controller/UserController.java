d785a8bcb5a54f7a7eef0fe69416e5f919afcef5
Synzenn <velriel42@gmail.com>
2026-04-22 21:52:13 +0700

    Menginput Library yang dibutuhkan

---
+++ src/main/java/PraktikumDB/_April/controller/UserController.java
@@ -0,0 +1,13 @@
+package PraktikumDB._April.controller;
+
+import PraktikumDB._April.Service.UserService;
+import PraktikumDB._April.model.User;
+import org.springframework.web.bind.annotation.*;
+import org.aspectj.lang.annotation.DeclareWarning;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+}
