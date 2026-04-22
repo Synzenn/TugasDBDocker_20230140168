95abd2789e4fab16a45660d6d7a6f8ad8cb22633
Synzenn <velriel42@gmail.com>
2026-04-22 21:53:09 +0700

    Menambahkan metode get, put, delete, dan post untuk crud

--- src/main/java/PraktikumDB/_April/controller/UserController.java
+++ src/main/java/PraktikumDB/_April/controller/UserController.java
@@ -9,5 +9,37 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
+    private final UserService userService;
 
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public String createUser(@RequestBody User request) {
+        userService.addUser(request);
+        return "User created successfully";
+    }
+
+    @GetMapping
+    public List<User> getAllUsers(){
+        return userService.getAllUser();
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteUser(@PathVariable String id) {
+        userService.deleteUser(id);
+        return "User deleted successfully";
+    }
+
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable String id) {
+        return userService.getUserById(id);
+    }
+
+    @PutMapping("/{id}")
+    public String updateUser(@PathVariable String id, @RequestBody User request) {
+        userService.updateUser(id, request);
+        return "user updated successfully";
+    }
 }
