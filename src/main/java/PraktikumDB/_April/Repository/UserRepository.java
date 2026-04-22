6044a1cd4a5c2cfbc700b6a74518f054799db96b
Synzenn <velriel42@gmail.com>
2026-04-22 21:54:11 +0700

    membuat UserRepository.java

---
+++ src/main/java/PraktikumDB/_April/Repository/UserRepository.java
@@ -0,0 +1,10 @@
+package PraktikumDB._April.Repository;
+
+import PraktikumDB._April.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, String> {
+
+}
